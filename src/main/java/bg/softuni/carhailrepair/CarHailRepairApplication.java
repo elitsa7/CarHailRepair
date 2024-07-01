b4d62a1cf08cb5e78004dd961f3c3e43eb067cf3
Elitsa Dacheva <122827000+elitsa7@users.noreply.github.com>
2024-07-01 22:00:37 +0300

    Initial commit

---
+++ src/main/java/bg/softuni/carhailrepair/CarHailRepairApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.carhailrepair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarHailRepairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarHailRepairApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bg/softuni/carhailrepair/CarHailRepairApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.carhailrepair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarHailRepairApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
